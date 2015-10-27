867fdd23c4be9b741ee898bc684d9ca216df963f
HeatherConnery <heatherconnery@gmail.com>
2015-10-27 12:12:08 -0400

    lecture finished

--- TableViewLecture/TableViewLecture/ViewController.swift
+++ TableViewLecture/TableViewLecture/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var myDataSource = ["Kwame","Johanna","Ethan","Elijah","Etienne"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myDataSource.count
+    }
+    
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = myDataSource[indexPath.row]
+        //Assignment read right to left (cell is built up reading left to right)
+        cell.detailTextLabel?.text = "Detailed text"
+        return cell
+    }
 
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
